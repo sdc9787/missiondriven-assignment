371c3dbb839dc091bf80a0d573c3ee1868bb83d8
sdc9787 <sfc9787@gmail.com>
2025-11-16 02:20:07 +0900

    fix : 날짜 포멧 변경

--- app/components/detail-info.tsx
+++ app/components/detail-info.tsx
@@ -76,7 +76,7 @@ export function DetailInfo() {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}.${month}.${day}`;
+    return `${year}년 ${month}월 ${day}일`;
   };
 
   // 세션 정보 업데이트 함수
